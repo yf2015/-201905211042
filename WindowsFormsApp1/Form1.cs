ce92af3cd17ab416ff651b61369ae35277bbb8e6
OYXF <984319355@qq.com>
2019-05-21 11:16:08 +0800

    这是一个项目

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image pic =  Image.FromStream(WebRequest.Create("https://lud4.cn/LINSHI/HTML53D/imgs/3.jpg").GetResponse().GetResponseStream());
+
+            this.pictureBox1.Image = pic;
+            this.pictureBox1.Height =  pic.Height;
+            this.pictureBox1.Width = pic.Width;
+        
+        }
+
+        private void Form1_BackgroundImageChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
